36feef92e093176b19ab5ff57c60f044c12b01d0
rosejtec <rosej.tec@gmail.com>
2020-12-18 22:12:37 +0800

    merged with main

--- backend/contracts/Buyers.sol
+++ backend/contracts/Buyers.sol
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.0;
+<<<<<<< HEAD
 
 import "./Structures.sol";
 contract Buyers is Structures {
     event Bidplaced(address indexed bidder, bytes32 productId, uint price);
+=======
+
+import "./Structures.sol";
+contract Buyers is Structures {
+    event BidPlacedEvent(address indexed bidder, bytes32 productId, uint price);
+
+>>>>>>> leah
 }
 
--- backend/contracts/Products.sol
+++ backend/contracts/Products.sol
@@ -3,23 +3,80 @@ pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 import "./Buyers.sol";
 import "./Sellers.sol";
-import "./Structures.sol";
-contract Products is Structures,Sellers { //TODO: import new holder contract (contains Seller and Buyer)
-  
-     mapping(bytes32 => Product) activeProducts; //make mapping, product id => product
-    mapping(address => Product[]) SellerToProduct; //add bool active to Product struct
 
-    function viewAllActiveContracts() public view returns(Product[] memory) { //add modifier Seller               
-         Product[] memory currAll = SellerToProduct[msg.sender] ;
+contract Products is Buyers,Sellers { //TODO: import new holder contract (contains Seller and Buyer)
+
+    mapping(bytes32 => Product) activeProducts;
+    bytes32[] activeProductIds;
+    mapping(address => Product[]) private sellerToProduct;
+    uint numOfProducts;
+
+    function addProduct(string memory name, string memory description, uint lowerBound, uint deadline) public {
+        bytes32 productID = keccak256(abi.encodePacked(name, description, deadline));
+        Bid memory emptyBid = Bid(address(0), 0, 0);
+        Product memory product = Product(productID, name, description, lowerBound, deadline, 0, emptyBid, true);
+        activeProducts[productID] = product;
+        sellerToProduct[msg.sender][sellerToProduct[msg.sender].length] = product;
+        activeProductIds[numOfProducts] = productID;
+        numOfProducts++;
+    }
+
+    function getProductDetailsById(bytes32 id) public view returns 
+        (string memory _name, string memory _description, uint _lowerBound, uint _deadline) 
+    {
+        Product memory product = activeProducts[id];
+        _name = product.name;
+        _description = product.description;
+        _lowerBound = product.lowerBound;
+        _deadline = product.deadline;
+    }
+
+    modifier isValidBid(bytes32 productId, uint price) {
+        Product storage currentProduct = activeProducts[productId];
+        require(currentProduct.isReal, "Product does not exist");
+        require(block.timestamp < currentProduct.deadline, "Auction time elapsed");
+        require(price > currentProduct.highestBid.bidPrice, "Bid price must be higher than current bid");
+        require(price >= currentProduct.lowerBound, "Bid price must be higher than lower bound");
+        _;
+    }
+
+    function placeBid(bytes32 productId, uint price) public payable isValidBid(productId, price) {
+        Product storage currentProduct = activeProducts[productId];
+        
+        Bid memory newBid = Bid({
+            bidder: msg.sender,
+            bidPrice: msg.value,
+            bidTime: block.timestamp
+        });
+
+        currentProduct.highestBid = newBid;
+        currentProduct.noOfBids += 1;
+
+        emit BidPlacedEvent(newBid.bidder, productId, price); 
+    }
+    
+    modifier onlySellers() {
+        require(sellerToProduct[msg.sender].length > 0 );
+        _;
+    }
+
+    function viewAllActiveProducts() onlySellers public view returns(Product[] memory)  { //add modifier Seller               
+         Product[] memory currAll = sellerToProduct[msg.sender] ;
          Product[] memory currActive;
 
          uint index = 0;
          for(uint i = 0 ; i<currAll.length; i++) {
-            if(activeProducts[currAll[i].id].id != 0){
-               currActive[index]=currAll[i];
-               index+=1;
+            if(activeProducts[currAll[i].id].isReal){
+               currActive[index] = currAll[i];
+               index += 1;
             }
          }
          return currActive;
-     }   
+     } 
+
+
+    function highestBid(bytes32 productId) public view returns(uint) { 
+         return activeProducts[productId].highestBid.bidPrice;
+    }   
+    
 }
