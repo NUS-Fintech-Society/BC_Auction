d2e988cc32a8f6b24712845e2b6a870269213173
Adithya Narayan Rangarajan Sreenivasan <e0426343@u.nus.edu>
2020-12-21 19:01:22 +0530

    Add deployment details and infura API keys

--- backend/contracts/Buyers.sol
+++ backend/contracts/Buyers.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.0;
-
 import "./Structures.sol";
+
 contract Buyers is Structures {
-        event BidPlaced(address indexed bidder, bytes32 productId, uint price);
+
+        event BidPlacedEvent(address indexed bidder, bytes32 productId, uint price);
+
 }
